92e8a44a552ad36eeebd194e3b3aac0c2dd16223
Nickolas Cunningham <nickolascunningham@gmail.c.com>
2021-09-30 15:07:44 -0400

    backend logic

--- server/server.js
+++ server/server.js
@@ -1,21 +1,56 @@
 // requires
-let express =  require( 'express' );
+let express = require('express');
 let app = express();
 
+app.use(express.json())
+
 // uses
-app.use( express.static( 'server/public' ) );
+app.use(express.static('server/public'));
 
 // globals
-const port = 5000;
+const port = 5001;
 
-// spin up server
-app.listen( port, ()=>{
-    console.log( 'server is up on', port);
-})
+let answer;
+let history = []
 
 // routes
-app.get( '/', (req, res)=>{
-    console.log( 'get route hit' );
-    res.send( 'Hello' );
+app.get('/answer', (req, res) => {
+    res.send({answer: answer});
 })
+
+app.get('/history', (req, res) => {
+    res.send(history)
+})
+
+//symbols and calculations
+app.post('/calculate', (req, res) => {
+    let { symbol, lastNum, firstNum } = req.body
+    if (symbol === "-") {
+        answer = firstNum - lastNum
+    } else if (symbol === "+") {
+        answer = firstNum + lastNum
+    } else if (symbol === "/") {
+        answer = firstNum / lastnum
+    } else if (symbol === "*") {
+        answer = firstNum * lastnum
+    } else {
+        answer = "invalid symbol"
+    }
+    const historyData = {
+        firstNum,
+        symbol,
+        lastNum,
+        answer
+    }
+    history.push(historyData)
+    res.send("OK")
+
+})
+
+
+// spin up server
+app.listen(port, () => {
+    console.log('server is up on', port);
+})
+
 
